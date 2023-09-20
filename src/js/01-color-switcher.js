9cc6ba6106294ace57cf6ad688f1ab2e6f0a843c
okaisym <okaisim@gmail.com>
2023-09-20 23:52:24 +0300

    fixed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,6 +3,7 @@ const btnStop = document.querySelector('[data-stop]');
 const body = document.querySelector('body');
 let timer = null;
 
+btnStop.setAttribute("disabled", false);
 
 btnStart.addEventListener('click', () => {
      timer = setInterval(() => {
